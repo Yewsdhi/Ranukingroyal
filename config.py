471bf7075a1646dbc8c638ab21869fc0b7e60026
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2024-11-15 12:40:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO", 
-    "https://github.com/Yewsdhi/Ranukingroyal/blob/main/README.md"
+    "https://github.com/Yewsdhi/Ranukingroyal"
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
